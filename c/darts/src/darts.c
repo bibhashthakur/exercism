c6cb24c1d448f8f0972d86b34adb759d97b9dc54
Bibhash Thakur <bibhashthakur92@gmail.com>
2020-05-10 23:55:08 +0530

    Complete 3rd exercise

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,1 +1,18 @@
 #include "darts.h"
+#include <math.h>
+#include <stdio.h>
+
+uint8_t score(coordinate_t position)
+{
+    double distance = pow(pow(position.x, 2) + pow(position.y, 2), 0.5);
+    //printf("%f", distance);
+
+    if (distance > 10)
+        return 0;
+    else if (distance > 5 && distance <= 10)
+        return 1;
+    else if (distance > 1 && distance <= 5)
+        return 5;
+    else
+        return 10;
+}
--- c/darts/test/test_darts.c
+++ c/darts/test/test_darts.c
@@ -11,7 +11,7 @@ void tearDown(void)
 
 static void test_missed_target(void)
 {
-   coordinate_t landing_position = { -9.0F, 9.0F };
+   coordinate_t landing_position = {-9.0F, 9.0F};
    uint8_t expected = 0;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -19,8 +19,8 @@ static void test_missed_target(void)
 
 static void test_on_the_outer_circle(void)
 {
-   TEST_IGNORE();               // delete this line to run test
-   coordinate_t landing_position = { 0.0F, 10.0F };
+   //TEST_IGNORE();               // delete this line to run test
+   coordinate_t landing_position = {0.0F, 10.0F};
    uint8_t expected = 1;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -28,8 +28,8 @@ static void test_on_the_outer_circle(void)
 
 static void test_on_the_middle_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -5.0F, 0.0F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-5.0F, 0.0F};
    uint8_t expected = 5;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -37,8 +37,8 @@ static void test_on_the_middle_circle(void)
 
 static void test_on_the_inner_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { 0.0F, -1.0F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {0.0F, -1.0F};
    uint8_t expected = 10;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -46,8 +46,8 @@ static void test_on_the_inner_circle(void)
 
 static void test_exactly_on_center(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { 0.0F, 0.0F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {0.0F, 0.0F};
    uint8_t expected = 10;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -55,8 +55,8 @@ static void test_exactly_on_center(void)
 
 static void test_near_the_center(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -0.1F, -0.1F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-0.1F, -0.1F};
    uint8_t expected = 10;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -64,8 +64,8 @@ static void test_near_the_center(void)
 
 static void test_just_within_the_inner_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { 0.7F, 0.7F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {0.7F, 0.7F};
    uint8_t expected = 10;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -73,8 +73,8 @@ static void test_just_within_the_inner_circle(void)
 
 static void test_just_outside_the_inner_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { 0.8F, -0.8F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {0.8F, -0.8F};
    uint8_t expected = 5;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -82,8 +82,8 @@ static void test_just_outside_the_inner_circle(void)
 
 static void test_just_within_the_middle_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -3.5F, 3.5F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-3.5F, 3.5F};
    uint8_t expected = 5;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -91,8 +91,8 @@ static void test_just_within_the_middle_circle(void)
 
 static void test_just_outside_the_middle_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -3.6F, -3.6F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-3.6F, -3.6F};
    uint8_t expected = 1;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -100,8 +100,8 @@ static void test_just_outside_the_middle_circle(void)
 
 static void test_just_within_the_outer_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -7.0F, 7.0F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-7.0F, 7.0F};
    uint8_t expected = 1;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -109,8 +109,8 @@ static void test_just_within_the_outer_circle(void)
 
 static void test_just_outside_the_outer_circle(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { 7.1F, -7.1F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {7.1F, -7.1F};
    uint8_t expected = 0;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
@@ -118,8 +118,8 @@ static void test_just_outside_the_outer_circle(void)
 
 static void test_asymmetric_position_between_the_inner_and_middle_circles(void)
 {
-   TEST_IGNORE();
-   coordinate_t landing_position = { -0.5F, -4.0F };
+   //TEST_IGNORE();
+   coordinate_t landing_position = {-0.5F, -4.0F};
    uint8_t expected = 5;
    uint8_t actual = score(landing_position);
    TEST_ASSERT_EQUAL_UINT8(expected, actual);
